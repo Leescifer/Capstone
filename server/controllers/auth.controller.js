f0082faa64d1d5bdfb65043e4b6fbed1b8c0e18e
Leecifer <128757198+Leescifer@users.noreply.github.com>
2025-03-17 12:49:30 +0800

    Successfuly send a message using mailtraper to gmail

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,7 +3,10 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const generateTokenAndSetCookies = (res, userId) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+         expiresIn: '7d'
+         });
+    
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
